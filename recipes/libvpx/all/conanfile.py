bc40f835af53a3c85f65dbf4551cb284813018b7
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2026-09-11 10:58:50 +0200

    fix libvpx asflags

--- recipes/libvpx/all/conanfile.py
+++ recipes/libvpx/all/conanfile.py
@@ -188,6 +188,10 @@ class LibVPXConan(ConanFile):
             env.define("CC", "")
         else:
             env = tc.environment()
+
+        if is_apple_os(self) and self.settings.arch in ['x86', 'x86_64']:
+            env.define("ASFLAGS", "")
+
         tc.generate(env)
 
     def _patch_sources(self):
